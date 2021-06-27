1fbdddc8ec64b794c543d019037cc387f52b1f46
Jaydeep <jaydeepshome06@gmai.com>
2021-06-27 16:04:00 +0530

    modified files

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,21 @@
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
+
+output "vpc_cidr" {
+  value = aws_vpc.vpc.cidr_block
+}
+
+output "public_subnets" {
+  value = {
+    for Subnet in aws_subnet.public :
+    Subnet.id => Subnet.cidr_block
+  }
+}
+
+output "private_subnets" {
+  value = {
+    for Subnet in aws_subnet.private :
+    Subnet.id => Subnet.cidr_block
+  }
+}
