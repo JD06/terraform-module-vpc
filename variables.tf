80d5bec2115fb39dbbc4c9c101f51cebada516fb
Jaydeep <jaydeepshome06@gmai.com>
2021-07-31 13:16:17 +0530

    changes to variable file

--- variables.tf
+++ variables.tf
@@ -14,21 +14,21 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet" {
-  type = map(number)
-  description = "Map of AZ to a number that should be used for public subnet"
+  type = map(string)
+  description = "Map of AZ to a string that should be used for public subnet"
   default = {
-    "public-subnet-2a" = 1
-    "public-subnet-2b" = 2
-    "public-subnet-2c" = 3
+    "public-subnet-2a" = "2a"
+    "public-subnet-2b" = "2b"
+    "public-subnet-2c" = "2c"
   }
 }
 
 variable "private_subnet" {
-  type = map(number)
-  description = "Map of AZ to a number that should be used for public subnet"
+  type = map(string)
+  description = "Map of AZ to a string that should be used for private subnet"
   default = {
-    "private-subnet-2a" = 4
-    "private-subnet-2b" = 5
-    "private-subnet-2c" = 6
+    "private-subnet-2a" = "2a"
+    "private-subnet-2b" = "2b"
+    "private-subnet-2c" = "2c"
   }
 }
